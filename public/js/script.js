436a72f907c4ef6b490a85cfe09008873bde926e
simarpreetsingh7 <samsingh7039.dtu@gmail.com>
2020-11-29 20:36:20 +0530

    peerjs+gitig

---
+++ public/js/script.js
@@ -0,0 +1,389 @@
+var socket = io({query:'loggeduser='+document.getElementById("myself").innerText+"&"+"meeting_id="+window.location.href.split("conversation/")[1]},{forceNew: false});
+
+socket.on('someone_joined', function(data) {
+    console.log(data.room_data.sockets)
+        var x=[];
+        var y=[];
+        x=[];
+        y=[];
+        var y = Object.keys(data.room_data.sockets);
+       for(var i=0;i<data.room_data.length;++i){x.push(data.decode[y[i]])} 
+console.log(x)
+
+    document.getElementById("dtext").innerHTML=x;
+})
+socket.on('someone_left', function(data) {
+
+    console.log(data.room_data.sockets)
+    var x=[];
+    var y=[];
+    x=[];
+    y=[];
+    var y = Object.keys(data.room_data.sockets);
+    for(var i=0;i<data.room_data.length;++i){x.push(data.decode[y[i]])} 
+    console.log(x)
+  
+    document.getElementById("dtext").innerHTML=x;
+})
+socket.on("other_listen",function(data){
+  document.getElementById("speaker").innerText=data.current_speaker+" is speaking";
+  document.getElementById("note-textarea").value+=data.current_speaker+" : "+data.msg+"           ";
+})
+
+
+  
+
+
+//zoom code start
+
+const videoGrid = document.getElementById('video-grid')
+//normal // const myPeer = new Peer();
+const myPeer = new Peer('samsingh', {
+  secure: true, 
+  host: 'dtumeet.herokuapp.com', 
+  port: 443,
+});
+//   undefined, {
+//   path: '/peerjs',
+//   host: '/',
+//   port: '443'
+// }) //heroku
+let myVideoStream;
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+  // input value
+  let text = $("input");
+  // when press enter send message
+  $('html').keydown(function (e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit('message', {message:text.val(),sender:document.getElementById("myself").innerText});
+      text.val('')
+    }
+  });
+  socket.on("createMessage", data => {
+    $("ul").append(`<li class="message"><b>${data.sender}</b> : ${data.message}</li>`);
+    scrollToBottom()
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+//zoom code end
+
+
+
+
+
+//speech engine code
+try {
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+  }
+  catch(e) {
+    console.error(e);
+    $('.no-browser-support').show();
+    $('.app').hide();
+  }
+  
+  
+  var noteTextarea = $('#note-textarea');
+  var instructions = $('#recording-instructions');
+  var notesList = $('ul#notes');
+  
+  var noteContent = '';
+  
+  // Get all notes from previous sessions and display them.
+  var notes = getAllNotes();
+  renderNotes(notes);
+  
+  
+  
+  /*-----------------------------
+        Voice Recognition 
+  ------------------------------*/
+  
+  // If false, the recording will stop after a few seconds of silence.
+  // When true, the silence period is longer (about 15 seconds),
+  // allowing us to keep recording even when the user pauses. 
+  recognition.continuous = true;
+  
+  // This block is called every time the Speech APi captures a line. 
+  recognition.onresult = function(event) {
+  
+    // event is a SpeechRecognitionEvent object.
+    // It holds all the lines we have captured so far. 
+    // We only need the current one.
+    var current = event.resultIndex;
+  
+    // Get a transcript of what was said.
+    var transcript = event.results[current][0].transcript;
+  
+    // Add the current transcript to the contents of our Note.
+    // There is a weird bug on mobile, where everything is repeated twice.
+    // There is no official solution so far so we have to handle an edge case.
+    var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+  
+    if(!mobileRepeatBug) {
+     // noteContent += transcript;
+    //  noteTextarea.val(noteContent);
+    }
+   
+     socket.emit("speaking",{current_speaker:document.getElementById("myself").innerText,msg:transcript})
+  };
+  
+  recognition.onstart = function() { 
+    instructions.text('Voice recognition activated. Try speaking into the microphone.');
+  }
+  
+  recognition.onspeechend = function() {
+    instructions.text('You were quiet for a while so voice recognition turned itself off.');
+  }
+  
+  recognition.onerror = function(event) {
+    if(event.error == 'no-speech') {
+      instructions.text('No speech was detected. Try again.');  
+    };
+  }
+  
+  
+  
+  /*-----------------------------
+        App buttons and input 
+  ------------------------------*/
+  
+  $('#start-record-btn').on('click', function(e) {
+    if (noteContent.length) {
+      noteContent += ' ';
+    }
+    
+    recognition.start();
+  });
+  
+  
+  $('#pause-record-btn').on('click', function(e) {
+    recognition.stop();
+    instructions.text('Voice recognition paused.');
+  });
+  
+  // Sync the text inside the text area with the noteContent variable.
+  noteTextarea.on('input', function() {
+    noteContent = $(this).val();
+  })
+  
+  $('#save-note-btn').on('click', function(e) {
+    recognition.stop();
+  
+    if(!noteContent.length) {
+      instructions.text('Could not save empty note. Please add a message to your note.');
+    }
+    else {
+      // Save note to localStorage.
+      // The key is the dateTime with seconds, the value is the content of the note.
+      saveNote(new Date().toLocaleString(), noteContent);
+  
+      // Reset variables and update UI.
+      noteContent = '';
+      renderNotes(getAllNotes());
+      noteTextarea.val('');
+      instructions.text('Note saved successfully.');
+    }
+        
+  })
+  
+  
+  notesList.on('click', function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+  
+    // Listen to the selected note.
+    if(target.hasClass('listen-note')) {
+      var content = target.closest('.note').find('.content').text();
+      readOutLoud(content);
+    }
+  
+    // Delete note.
+    if(target.hasClass('delete-note')) {
+      var dateTime = target.siblings('.date').text();  
+      deleteNote(dateTime);
+      target.closest('.note').remove();
+    }
+  });
+  
+  
+  
+  /*-----------------------------
+        Speech Synthesis 
+  ------------------------------*/
+  
+  function readOutLoud(message) {
+      var speech = new SpeechSynthesisUtterance();
+  
+    // Set the text and voice attributes.
+      speech.text = message;
+      speech.volume = 1;
+      speech.rate = 1;
+      speech.pitch = 1;
+    
+      window.speechSynthesis.speak(speech);
+  }
+  
+  
+  
+  /*-----------------------------
+        Helper Functions 
+  ------------------------------*/
+  
+  function renderNotes(notes) {
+    var html = '';
+    if(notes.length) {
+      notes.forEach(function(note) {
+        html+= `<li class="note">
+          <p class="header">
+            <span class="date">${note.date}</span>
+            <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
+            <a href="#" class="delete-note" title="Delete">Delete</a>
+          </p>
+          <p class="content">${note.content}</p>
+        </li>`;    
+      });
+    }
+    else {
+      html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
+    }
+    notesList.html(html);
+  }
+  
+  
+  function saveNote(dateTime, content) {
+    localStorage.setItem('note-' + dateTime, content);
+  }
+  
+  
+  function getAllNotes() {
+    var notes = [];
+    var key;
+    for (var i = 0; i < localStorage.length; i++) {
+      key = localStorage.key(i);
+  
+      if(key.substring(0,5) == 'note-') {
+        notes.push({
+          date: key.replace('note-',''),
+          content: localStorage.getItem(localStorage.key(i))
+        });
+      } 
+    }
+    return notes;
+  }
+  
+  
+  function deleteNote(dateTime) {
+    localStorage.removeItem('note-' + dateTime); 
+  }
+  
